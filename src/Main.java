5f73d2e8b443e730d78c7000bda1ced19152755e
Andrew Meleshnikov <andreymeleshnikov@gmail.com>
2022-08-24 12:30:24 +0300

    task1 added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
     }
+
+    public static void task1() {
+        System.out.println("Task1");
+        for (int i = 0; i < 3000; i++) {
+            printYearIsLeap(i);
+        }
+    }
+
+    public static void printYearIsLeap(int year) {
+        boolean isLeapYear = ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0));
+        String message = "год не является високосным";
+        if (isLeapYear) {
+            message = "год является вискосным";
+        }
+        System.out.printf("%d %s\n", year, message);
+    }
+
 }
