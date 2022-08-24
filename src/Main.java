db08d2d41cfc5dcc3e25f044d36783ccd1d2cb73
Andrew Meleshnikov <andreymeleshnikov@gmail.com>
2022-08-24 13:10:07 +0300

    task2 added

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        task1();
+        //task1();
+        task2();
     }
 
     public static void task1() {
@@ -10,6 +13,14 @@ public class Main {
         }
     }
 
+    public static void task2() {
+        printClientOs(0, 2001);
+        printClientOs(0, 2024);
+        printClientOs(1, 2005);
+        printClientOs(1, 2034);
+        printClientOs(5, 2022);
+    }
+
     public static void printYearIsLeap(int year) {
         boolean isLeapYear = ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0));
         String message = "год не является високосным";
@@ -17,6 +28,24 @@ public class Main {
             message = "год является вискосным";
         }
         System.out.printf("%d %s\n", year, message);
+    }
+
+    public static void printClientOs(int clientOS, int clientDeviceYear) {
+        int currentYear = LocalDate.now().getYear();
+        String liteVersion = (clientDeviceYear < currentYear) ? "облегченную " : "";
+        String os = "";
+        switch (clientOS) {
+            case 0:
+                os = "iOS";
+                break;
+            case 1:
+                os = "Android";
+                break;
+            default:
+                System.out.println("Такой версии ОС нет в базе");
+                return;
+        }
+        System.out.printf("Установите %sверсию приложения для %s по ссылке\n", liteVersion, os);
     }
 
 }
